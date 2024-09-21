748689f1c38914cdeac2985afdcdf44d76c85567
Salviavi <entp7w8@gmail.com>
2024-09-21 20:24:04 +0700

    updating food api in index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,11 @@
 import { getCookie } from "cookies-next";
 import { redirect } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export const getServerSideProps = (context) => {
+    const [foods, setFoods] = useState([]);
+    const [loading, setLoading] = useState(false);
+
     const token = getCookie("token", context)
    
     if (!token) {
@@ -9,12 +13,33 @@ export const getServerSideProps = (context) => {
     }
 
     return {props: {}}
+
+    useEffect(() => {
+      const getData = async () => {
+        setLoading(true);
+        const resp = await axios.get("https://api-bootcamp.do.dibimbing.id/api/v1/foods", {
+          headers: { apiKey: "w05KkI9AWhKxzvPFtXotUva-"},
+        })
+
+        setFoods(resp?.data?.data);
+        setLoading(false);
+      }
+      getData();
+    }, [])
+
+    if (loading) return <div>Loading...</div>;
 }
 
 const hehe = () => {
     return (
         <div>
-            halo
+            <ul className="mx-auto grid justify-center">
+              {foods.map((food) => {
+                <li className="mb-8">
+                  <img src={food.imageUrl} className="w-96 object-cover aspect-video" />
+                </li>
+              })}
+            </ul>
         </div>
     )
 }
